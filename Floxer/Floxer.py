11ea4186d61daa6dab6710682b6de05c577e1057
M10207432 <M10207432@mail.ntust.edu.tw>
2016-07-07 01:21:05 +0800

    Mouse event

--- Floxer/Floxer.py
+++ Floxer/Floxer.py
@@ -8,9 +8,20 @@ class Floxer(gtk.Window):
         win.connect("delete-event", gtk.main_quit)  #kill thread
         win.set_opacity(0.5)                        #set win opacity [0:1]
 
+        win.add_events(gtk.gdk.BUTTON_PRESS_MASK)   #set mouse event
+        win.connect('button-press-event',self.mouse_callback)
+        
+        
         win.show_all()
         gtk.main()
 
+
+    def move(self,x,y):
+        pass
+
+    def mouse_callback(self,window,event):
+        print "x=", event.x, "y=", event.y
+        
 def main():
     print "Gadget Start !!!"
 
