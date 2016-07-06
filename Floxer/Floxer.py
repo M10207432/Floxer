7091547e3767c2175280be6fc4ca3d7dd3c7217a
M10207432 <M10207432@mail.ntust.edu.tw>
2016-07-07 00:49:54 +0800

    Add class doc and revise to class

--- Floxer/Floxer.py
+++ Floxer/Floxer.py
@@ -1,6 +1,14 @@
 import gtk
 #import webkit
 
+class Floxer(gtk.Window):
+    def __init__(self,win):
+        #win=gtk.Window()
+        win.set_decorated(False)                    #hide title bar
+        win.connect("delete-event", gtk.main_quit)  #kill thread
+        win.show_all()
+        gtk.main()
+
 def main():
     print "Gadget Start !!!"
     print "Using GTK"
@@ -8,11 +16,18 @@ def main():
     '''==================
         Show Window
     =================='''
+
+    '''
     win=gtk.Window()
-    win.connect("delete-event", gtk.main_quit) #kill thread
+    win.set_decorated(False)                    #hide title bar
+    win.connect("delete-event", gtk.main_quit)  #kill thread
     win.show_all()
     gtk.main()
+    '''
+    win=gtk.Window()
+    F=Floxer(win)
 
+    
     '''==================
     =================='''
     
