8c9af0f831d27ac2a2630bc2b95e73c0a822989a
M10207432 <M10207432@mail.ntust.edu.tw>
2016-07-07 00:23:51 +0800

    Gtk install and first view

--- Floxer/Floxer.py
+++ Floxer/Floxer.py
@@ -1,7 +1,21 @@
+import gtk
+#import webkit
 
 def main():
-    print "Start"
+    print "Gadget Start !!!"
+    print "Using GTK"
 
+    '''==================
+        Show Window
+    =================='''
+    win=gtk.Window()
+    win.connect("delete-event", gtk.main_quit) #kill thread
+    win.show_all()
+    gtk.main()
+
+    '''==================
+    =================='''
+    
 
 if __name__=="__main__":
-        main()
+    main()
