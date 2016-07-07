34afcdda49b15fdc5eaa9485b934629b3b66413f
M10207432 <M10207432@mail.ntust.edu.tw>
2016-07-07 14:34:24 +0800

    Drag and motion detect Done

--- Floxer/Floxer.py
+++ Floxer/Floxer.py
@@ -6,24 +6,25 @@ class Floxer(gtk.Window):
         
         win.set_decorated(False)                    #hide title bar
         win.connect("delete-event", gtk.main_quit)  #kill thread
-        win.set_opacity(0.5)                        #set win opacity [0:1]
+        win.set_opacity(1)                          #set win opacity [0:1]
+        
+        win.add_events(gtk.gdk.POINTER_MOTION_MASK | gtk.gdk.BUTTON_PRESS_MASK)     #set event mask
+        win.connect('motion_notify_event',self.motion_cb)                           #set mouse motion to callback function
+        win.connect('button_press_event',self.click_cb)                             #set mouse click to callback function
 
-        win.add_events(gtk.gdk.POINTER_MOTION_MASK )            #set event mask
-        win.connect('motion_notify_event',self.motion_cb)       #set signal to function
-
+        self.drag_flag=True
+        
         self.win=win
         win.show_all()
         gtk.main()
 
-    def move(self,x,y):
-        pass
+    def click_cb(self,window,event):
+        if event.button==1 & self.drag_flag:
+            self.win.begin_move_drag(event.button, int(event.x_root),int(event.y_root),event.time) #Drag Function
 
     def motion_cb(self,window,event):
-
-        if event.state & gtk.gdk.BUTTON1_MASK:        #Press state in motion situation
-            print "x=", event.x, "y=", event.y
+        print event.x, event.y
         
-                
 def main():
     print "Gadget Start !!!"
 
