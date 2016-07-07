32f8aeb553a7ad5dcdf9028530dd07a3832eae3b
M10207432 <M10207432@mail.ntust.edu.tw>
2016-07-07 15:06:20 +0800

    Reorder code and add img

--- Floxer/Floxer.py
+++ Floxer/Floxer.py
@@ -3,20 +3,43 @@ import gtk
 
 class Floxer(gtk.Window):
     def __init__(self,win):
-        
-        win.set_decorated(False)                    #hide title bar
-        win.connect("delete-event", gtk.main_quit)  #kill thread
-        win.set_opacity(1)                          #set win opacity [0:1]
-        
-        win.add_events(gtk.gdk.POINTER_MOTION_MASK | gtk.gdk.BUTTON_PRESS_MASK)     #set event mask
-        win.connect('motion_notify_event',self.motion_cb)                           #set mouse motion to callback function
-        win.connect('button_press_event',self.click_cb)                             #set mouse click to callback function
 
-        self.drag_flag=True
-        
+        '''--------------------
+            Variable
+        --------------------'''
         self.win=win
-        win.show_all()
-        gtk.main()
+        self.gtk=gtk
+        
+        '''--------------------
+            Setting
+        --------------------'''
+        self.drag_flag=True             #Drag Windows flag
+        self.win.set_decorated(False)   #hide title bar
+        self.win.set_opacity(0.5)       #set win opacity [0:1]
+
+        '''--------------------
+            Event Callback
+        --------------------'''
+        self.win.add_events(self.gtk.gdk.POINTER_MOTION_MASK | self.gtk.gdk.BUTTON_PRESS_MASK)     #set event mask
+        self.win.connect('motion_notify_event',self.motion_cb)              #set mouse motion to callback function
+        self.win.connect('button_press_event',self.click_cb)                #set mouse click to callback function
+        self.win.connect("delete-event", self.gtk.main_quit)                #kill thread event
+        
+        '''--------------------
+            Function
+        --------------------'''
+        self.Set_Widget()       #Widget Function
+
+        #=================Start
+        self.win.show_all()
+        self.gtk.main()
+
+    def Set_Widget(self):
+        #=====Image
+        img=self.gtk.Image()
+        img.set_from_file("fire.png")
+        self.win.add(img)
+
 
     def click_cb(self,window,event):
         if event.button==1 & self.drag_flag:
@@ -34,7 +57,6 @@ def main():
     win=gtk.Window()
     F=Floxer(win)
 
-    
     '''==================
     =================='''
     
